deafcd98df31dcbb40b4cd202a7014264b64c12c
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-09 21:48:45 +0300

    Creact Redux Store

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from 'redux';
+import todoApp from './reducers';
+
+const store = createStore(todoApp);
+
 ReactDOM.render(<App />, document.getElementById('root'));
