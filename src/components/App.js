25c08eb17aab4b679404be9b890c945920c006e4
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-14 12:19:24 +0300

    add Todo component with css styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import './App.css';
+import Todo from './Todo';
+
 class App extends Component {
     state = {todos: []};
 
@@ -22,6 +25,7 @@ class App extends Component {
                     type="text"
                     onChange={this.changeHandler}
                 />
+                <Todo/>
             </div>
         );
     }
---
+++ src/components/Todo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Todo.css';
+
+const Todo = () => (
+    <div className="Todo">
+        <p>1810141202 Add styles to component</p>
+    </div>
+);
+
+export default Todo;
