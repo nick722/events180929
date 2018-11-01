40b869f1cac2dd5410b6c269ea1a848d7f1a06ef
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-11-01 17:56:01 +0300

    WIP fetch todos from Google Tasks

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import Layout from "./Layout";
 import TodoList from "../containers/TodoList";
 import AddTodo from "../containers/AddTodo";
-import Reddit from "../containers/fetchDataFromAServer";
+import FetchTodosFromGoogleTasks from "../containers/FetchTodosFromGoogleTasks";
 
 class App extends Component {
   render() {
@@ -15,7 +15,7 @@ class App extends Component {
           <TodoList />
         </Layout>
         <hr />
-        <Reddit />
+        <FetchTodosFromGoogleTasks />
       </div>
     );
   }
--- src/containers/fetchDataFromAServer/index.js
+++ src/containers/fetchDataFromAServer/index.js
@@ -1,37 +1,37 @@
-/**
- * Exercises from Dave Ceddia's [Pure React] Lesson 4: Fetch data from a server
- * https://mail.google.com/mail/u/0/#label/Frontend%2FDave+Ceddia/FMfcgxvzKtTNdqTpMMFfkpPVGKVzqbzp
- */
-
-import React, { Component } from "react";
-import axios from "axios";
-
-class Reddit extends Component {
-  state = {
-    posts: []
-  };
-
-  componentDidMount() {
-    axios.get(`https://www.reddit.com/r/reactjs.json`).then(res => {
-      const posts = res.data.data.children.map(obj => obj.data);
-      this.setState({ posts });
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Reddit</h3>
-        <ul>
-          {this.state.posts.map(post => (
-            <li key={post.id}>
-              <a href={post.url}>{post.title}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
-export default Reddit;
+// /**
+//  * Exercises from Dave Ceddia's [Pure React] Lesson 4: Fetch data from a server
+//  * https://mail.google.com/mail/u/0/#label/Frontend%2FDave+Ceddia/FMfcgxvzKtTNdqTpMMFfkpPVGKVzqbzp
+//  */
+//
+// import React, { Component } from "react";
+// import axios from "axios";
+//
+// class Reddit extends Component {
+//   state = {
+//     posts: []
+//   };
+//
+//   componentDidMount() {
+//     axios.get(`https://www.reddit.com/r/reactjs.json`).then(res => {
+//       const posts = res.data.data.children.map(obj => obj.data);
+//       this.setState({ posts });
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h3>Reddit</h3>
+//         <ul>
+//           {this.state.posts.map(post => (
+//             <li key={post.id}>
+//               <a href={post.url}>{post.title}</a>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default Reddit;
