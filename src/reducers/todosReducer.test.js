1c2588d595e7b46330fcf8c22775ffaf1840b65a
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-11-01 12:49:08 +0300

    add "todosReducer should handle ADD_TODO action" test

--- src/reducers/todosReducer.test.js
+++ src/reducers/todosReducer.test.js
@@ -1,8 +1,23 @@
 import todosReducer from "./todosReducer";
 import * as types from "../constants/ActionTypes";
 
-describe("todosReducer reducer", () => {
+describe("todosReducer", () => {
   it("should return the initial state", () => {
     expect(todosReducer(undefined, {})).toEqual([]);
+  });
+
+  it("should handle ADD_TODO action", () => {
+    expect(
+      todosReducer([], {
+        type: types.ADD_TODO,
+        text: "Add tests for reducers"
+      })
+    ).toEqual([
+      {
+        isDone: false,
+        task: "Add tests for reducers",
+        id: 1
+      }
+    ]);
   });
 });
